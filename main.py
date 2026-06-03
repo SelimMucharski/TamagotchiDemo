8041b29ba864e63a8f27e8b98f7e50203847d95b
SelimMucharski <mucharski.selim@gmail.com>
2026-06-03 16:25:50 +0000

    Added fly effect

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import pygame
 from utils import *
-from Pet import PetSprite, ShadowSprite
+from Pet import PetSprite, ShadowSprite, HeartEffect, FlyEffect
 from Background import Background
+import random
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Sprite Groups")
+pygame.display.set_caption("Hello Clean")
 
 clock = pygame.time.Clock()
 FPS = 24
+
+
+def addHeartToPet(sprites, pet):
+    sprites.add(
+        FlyEffect(
+            pet.pos.x + random.uniform(-10, 10),
+            pet.pos.y + pet.size + random.uniform(-10, 10) - 10
+        )
+    )
 
 
 all_sprites = pygame.sprite.Group()
@@ -23,10 +33,15 @@ shadow = ShadowSprite(pet)
 all_sprites.add(background)
 all_sprites.add(shadow)
 all_sprites.add(pet)
+all_sprites.remove()
+
+HEART_EVENT = pygame.USEREVENT + 1
+
+pygame.time.set_timer(HEART_EVENT, 500)
+
 
 run = True
 while run:
-
     clock.tick(FPS)
 
     screen.fill("cyan")
@@ -38,6 +53,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        if event.type == HEART_EVENT:
+            addHeartToPet(all_sprites, pet)
 
     pygame.display.flip()
 
