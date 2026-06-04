c8ec9f3606c006baa9bb9975730f80c9e9461eb5
SelimMucharski <mucharski.selim@gmail.com>
2026-06-04 16:28:02 +0000

    Started top menu

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame
+import pygame_gui
+
 from utils import *
 from Pet import PetSprite, ShadowSprite, Pet
 import Food
-
 from Effects import *
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 pygame.display.set_caption("Hello Clean")
 
 clock = pygame.time.Clock()
@@ -17,38 +20,73 @@ all_sprites = pygame.sprite.Group()
 foods = pygame.sprite.Group()
 
 pet = Pet(0, 0)
-
 pet_sprite = PetSprite(pet)
-
 shadow = ShadowSprite(pet)
 
 all_sprites.add(shadow)
 all_sprites.add(pet_sprite)
 
 background_image = pygame.image.load(
-    "assets/background/background.png").convert()
+    "assets/background/background.png"
+).convert()
 
 background_surface = pygame.transform.scale(
-    background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    background_image, (SCREEN_WIDTH, SCREEN_HEIGHT)
+)
+
+menu_expanded = False
+
+settings_image = pygame.image.load("assets/gui/plus.png").convert_alpha()
+settings_image = pygame.transform.smoothscale(settings_image, (50, 50))
+
+settings_icon = pygame_gui.elements.UIImage(
+    relative_rect=pygame.Rect((SCREEN_WIDTH - 60, 10), (50, 50)),
+    image_surface=settings_image,
+    manager=manager
+)
+
+menu_buttons = []
+
+for i, text in enumerate(["Food", "Save", "Debug"]):
+    btn = pygame_gui.elements.UIImage(
+        relative_rect=pygame.Rect(
+            (SCREEN_WIDTH - 150 - i * 60, 10),
+            (50, 50)
+        ),
+        image_surface=settings_image,
+        manager=manager
+    )
+    btn.hide()
+    menu_buttons.append(btn)
+
+GUI_RECT = pygame.Rect(0, 0, SCREEN_WIDTH, 60)
+
+
+def is_pointer_over_gui(x, y):
+    return GUI_RECT.collidepoint(x, y)
+
+
+def spawn_food(world_x, world_y):
+    food_entity = Food.RandomFood(world_x, world_y)
+    all_sprites.add(food_entity)
+    foods.add(food_entity)
+
+
+def finger_to_screen(event):
+    return (
+        event.x * SCREEN_WIDTH,
+        event.y * SCREEN_HEIGHT
+    )
 
 
 run = True
 while run:
-    clock.tick(FPS)
-
-    all_sprites.update()
-
-    screen.blit(background_surface, (0, 0))
-    all_sprites.draw(screen)
-
-    hits = pygame.sprite.spritecollide(pet_sprite, foods, True)
-
-    for food in hits:
-        pet.eat(food)
-
-    pet.update(None)
+    time_delta = clock.tick(FPS) / 1000.0
 
     for event in pygame.event.get():
+
+        manager.process_events(event)
+
         if event.type == pygame.QUIT:
             run = False
 
@@ -58,29 +96,80 @@ while run:
         if event.type == ITEM_ON_GROUND_EVENT:
             pet.go_to(event.pos.x)
 
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+
+            if event.ui_element == menu_buttons[0]:
+                print("Food")
+
+            elif event.ui_element == menu_buttons[1]:
+                print("Save")
+
+            elif event.ui_element == menu_buttons[2]:
+                print("Debug")
+
         if event.type == pygame.MOUSEBUTTONDOWN:
-            screen_x, screen_y = pygame.mouse.get_pos()
-            x, y = screen_to_word(screen_x, screen_y)
-            food_entity = Food.RandomFood(x, y)
 
-            all_sprites.add(food_entity)
-            foods.add(food_entity)
+            mx, my = event.pos
 
-        if event.type == pygame.FINGERDOWN:
-            if event.x == 0 and event.y == 0:
+            # klik w ikonę settings
+            if settings_icon.rect.collidepoint(mx, my):
+                menu_expanded = not menu_expanded
+
+                if menu_expanded:
+                    settings_icon.set_image(pygame.image.load(
+                        "assets/gui/cross.png").convert_alpha())
+                else:
+                    settings_icon.set_image(pygame.image.load(
+                        "assets/gui/plus.png").convert_alpha())
+
+                for btn in menu_buttons:
+                    if menu_expanded:
+                        btn.show()
+                    else:
+                        btn.hide()
+
                 continue
 
-            screen_x = (1-event.y) * screen.get_width()
-            screen_y = event.x * screen.get_height()
+            if is_pointer_over_gui(mx, my):
+                continue
 
-            x, y = screen_to_word(screen_x, screen_y)
+            world_x, world_y = screen_to_word(mx, my)
+            spawn_food(world_x, world_y)
 
-            food_entity = Food.RandomFood(x, y)
-            all_sprites.add(food_entity)
-            foods.add(food_entity)
+        if event.type == pygame.FINGERDOWN:
 
-            #print(screen_x, screen_y)
-            #print(screen_x, screen_y)
+            mx, my = finger_to_screen(event)
+
+            if settings_icon.rect.collidepoint(mx, my):
+                menu_expanded = not menu_expanded
+
+                for btn in menu_buttons:
+                    if menu_expanded:
+                        btn.show()
+                    else:
+                        btn.hide()
+
+                continue
+
+            if is_pointer_over_gui(mx, my):
+                continue
+
+            world_x, world_y = screen_to_word(mx, my)
+            spawn_food(world_x, world_y)
+
+    manager.update(time_delta)
+
+    hits = pygame.sprite.spritecollide(pet_sprite, foods, True)
+    for food in hits:
+        pet.eat(food)
+
+    pet.update(None)
+    all_sprites.update()
+
+    screen.blit(background_surface, (0, 0))
+    all_sprites.draw(screen)
+
+    manager.draw_ui(screen)
 
     pygame.display.flip()
 
